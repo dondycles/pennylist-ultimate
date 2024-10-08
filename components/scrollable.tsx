8cb9b235b5f26165dcedaaf55d8dd75f6538b4ae
John Rod Dondoyano <johnrod.dondoyano@gmail.com>
2024-10-09 00:45:11 +0800

    Update scrollable.tsx

--- components/scrollable.tsx
+++ components/scrollable.tsx
@@ -4,7 +4,7 @@ export default function Scrollable({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex-1 flex flex-col overflow-auto max-w-[800px] w-screen mx-auto gap-4 pb-32">
+    <div className="flex-1 flex flex-col overflow-auto max-w-[800px] w-screen mx-auto pb-32">
       {children}
     </div>
   );
