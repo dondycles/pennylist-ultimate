78a72aa022af3e742025a0cddf4076625e19d187
dondycles <johnrod.dondoyano@gmail.com>
2024-10-22 11:06:10 +0800

    modified:   app/(user)/list/money/[id]/page.tsx

--- app/(user)/list/money/[id]/page.tsx
+++ app/(user)/list/money/[id]/page.tsx
@@ -1,9 +1,8 @@
 import MoneyComponent from "./money";
 
-export default async function MoneyPage({
-  params,
-}: {
-  params: { id: string };
+export default async function MoneyPage(props: {
+  params: Promise<{ id: string }>;
 }) {
+  const params = await props.params;
   return <MoneyComponent id={params.id} />;
 }
