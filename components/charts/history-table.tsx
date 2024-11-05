092654304f5e5e193c5012aa80d4018adf7f26e2
dondycles <johnrod.dondoyano@gmail.com>
2024-11-05 18:04:39 +0800

    modified:   components/charts/history-table.tsx

--- components/charts/history-table.tsx
+++ components/charts/history-table.tsx
@@ -40,6 +40,8 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Log, Money } from "@/store";
+import { Badge } from "../ui/badge";
+import Amount from "../amount";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -65,7 +67,7 @@ export function HistoryTable<TData, TValue>({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
+    // getPaginationRowModel: getPaginationRowModel(),
     onColumnFiltersChange: setColumnFilters,
     getFilteredRowModel: getFilteredRowModel(),
     onSortingChange: setSorting,
@@ -181,13 +183,74 @@ export function HistoryTable<TData, TValue>({
             table.getRowModel().rows.map((row) => {
               const log = row.original as Log;
               return (
-                <div key={row.id} className="p-4 rounded-xl bg-muted">
+                <div
+                  key={row.id}
+                  className="p-4 rounded-2xl bg-muted/50 flex flex-col gap-2"
+                >
                   {/* {row.getVisibleCells().map((cell) => (
                   <TableCell key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))} */}
-                  {log.changes.latest.name}
+                  <div className="flex gap-4 items-start justify-between">
+                    <Badge className="capitalize">{log.action}</Badge>
+                    <p className="text-xs text-muted-foreground">
+                      {new Date(log.created_at).toLocaleDateString()}
+                    </p>
+                  </div>
+                  <div
+                    style={{ color: log.changes.latest.color }}
+                    className="flex flex-col items-center truncate w-full"
+                  >
+                    <p className="text-sm font-bold">
+                      {log.changes.latest.name}
+                    </p>
+                    <div className="flex items-baseline gap-1">
+                      <Amount
+                        amount={log.changes.prev.amount}
+                        settings={{ sign: true }}
+                        className="text-xs text-muted-foreground"
+                      />
+                      <div className="flex gap-1">
+                        <p className="text-xl font-semibold">
+                          {log.changes.latest.amount - log.changes.prev.amount >
+                          0
+                            ? "+"
+                            : "-"}
+                        </p>
+                        <Amount
+                          amount={log.changes.latest.amount}
+                          settings={{ sign: true }}
+                          className="text-xl"
+                        />
+                        <p className="text-xl font-semibold">=</p>
+                      </div>
+                      <div>
+                        <Amount
+                          amount={Math.abs(
+                            log.changes.latest.amount - log.changes.prev.amount
+                          )}
+                          settings={{ sign: true }}
+                          className="text-xs text-muted-foreground"
+                        />
+                      </div>
+                    </div>
+
+                    <div className="flex items-baseline text-muted-foreground text-xs gap-1">
+                      <p>Total Money: </p>
+                      <Amount
+                        className="text-sm"
+                        amount={log.current_total}
+                        settings={{ sign: true }}
+                      />
+                    </div>
+                  </div>
+                  <p
+                    hidden={!log.reason}
+                    className="text-sm text-muted-foreground"
+                  >
+                    {log.reason}
+                  </p>
                 </div>
               );
             })
@@ -195,7 +258,7 @@ export function HistoryTable<TData, TValue>({
             <p>No results.</p>
           )}
         </div>
-        <div className="flex flex-row justify-center gap-4 mt-4">
+        {/* <div className="flex flex-row justify-center gap-4 mt-4">
           <Button
             variant="outline"
             size="icon"
@@ -214,7 +277,7 @@ export function HistoryTable<TData, TValue>({
           >
             <ArrowRightIcon className="h-4 w-4" />
           </Button>
-        </div>
+        </div> */}
       </CardContent>
     </Card>
   );
