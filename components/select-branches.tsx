f0f7eeed06cb22d3280010ff026ec3e4c38dd247
dondycles <johnrod.dondoyano@gmail.com>
2024-11-06 00:41:41 +0800

    modified:   components/select-branches.tsx

--- components/select-branches.tsx
+++ components/select-branches.tsx
@@ -8,12 +8,17 @@ export default function SelectBranches() {
 
   if (transferrings)
     return (
-      <div className="grid grid-cols-2 md:grid-cols-3  gap-4 p-4">
-        {moneysStore.moneys
-          .filter((m) => m.id !== transferrings?.root.id)
-          .map((m) => {
-            return <MoneysTransferCard m={m} key={m.id} />;
-          })}
+      <div className="flex flex-col pt-4">
+        <p className="text-sm text-center text-muted-foreground">
+          Select the receiving ends
+        </p>
+        <div className="grid grid-cols-2 md:grid-cols-3  gap-4 p-4">
+          {moneysStore.moneys
+            .filter((m) => m.id !== transferrings?.root.id)
+            .map((m) => {
+              return <MoneysTransferCard m={m} key={m.id} />;
+            })}
+        </div>
       </div>
     );
   return null;
